dfddcf569eb6cd0ecaa0dc2ccac4288e514510bd
Slippy Douglas <git@6bitt.com>
2010-05-31 18:14:40 -0700

    Updated MooTools core (from 1.2 to 1.2.4) and more (from 1.2 to 1.2.4.4 with all extras except only the default localization).

--- lib/rails/asset_tag_helper.rb
+++ lib/rails/asset_tag_helper.rb
@@ -1,20 +1,22 @@
 module ActionView::Helpers::AssetTagHelper
   alias_method :rails_javascript_include_tag, :javascript_include_tag
   
-  JAVASCRIPT_DEFAULT_SOURCES = ['mootools-1.2-core', 'mootools-1.2-more', 'application', 'mootools_patch']
+  MOOTOOLS_CORE_SOURCE = 'mootools-1.2.4-core'
+  MOOTOOLS_MORE_SOURCE = 'mootools-1.2.4.4-more'
+  JAVASCRIPT_DEFAULT_SOURCES = [MOOTOOLS_CORE_SOURCE, MOOTOOLS_MORE_SOURCE, 'application', 'mootools_patch']
   @@javascript_default_sources = JAVASCRIPT_DEFAULT_SOURCES.dup
     
   def javascript_include_tag(*sources)
-   if sources.delete :mootools
-     sources = sources.concat(
-       ['mootools-1.2-core', 'mootools-1.2-more', 'application', behaviours_url]
-     ).uniq
-   elsif sources.delete :defaults
-     sources = ['mootools-1.2-core', 'mootools-1.2-more', 'mootools_patch', 'application', behaviours_url].concat(sources)
-   end
-
-   
-   rails_javascript_include_tag(*sources)
+    if sources.delete :mootools
+      sources = sources.concat(
+        [MOOTOOLS_CORE_SOURCE, MOOTOOLS_MORE_SOURCE, 'application', behaviours_url]
+      ).uniq
+    elsif sources.delete :defaults
+      sources = [MOOTOOLS_CORE_SOURCE, MOOTOOLS_MORE_SOURCE, 'mootools_patch', 'application', behaviours_url].concat(sources)
+    end
+    
+    
+    rails_javascript_include_tag(*sources)
   end
   
   protected  
